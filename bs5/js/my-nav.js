3f41404e06bd160801a3a2638aa1e748acfc4757
didier <didier@d-defrance.fr>
2021-08-09 10:14:52 +0200

    avec bootstrap 5

---
+++ bs5/js/my-nav.js
@@ -0,0 +1,7 @@
+       function switchContent(content_subpage){
+		  //console.log("switchContent : " + content_subpage);
+	      document.querySelector('#content-div-id').innerHTML=content_subpage;
+		  //version basique à améliorer (SPA , ajax, ...)
+	   }
+
+	   
